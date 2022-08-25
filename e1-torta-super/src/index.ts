c14499c0158e6557c581d6ae4645103eeb8a00f4
LopezMailen <110505288+LopezMailen@users.noreply.github.com>
2022-08-25 11:13:25 -0300

    SUBI EJERCICIO 1

---
+++ e1-torta-super/src/index.ts
@@ -0,0 +1,27 @@
+// EJERCICION TORTA
+var cocinarTorta = [
+  "Torta de vainilla",
+  "1-En un bol Colocas los huevos,el azúcar,el aceite,la leche",
+  "2-Batir a mano pero no mucho",
+  "3-Agregas  una cucharada de vainilla ",
+  "4-Agregar Harina",
+  "5-Hornear por 35/40' a 180°"
+];
+
+for (let i = 0; i < cocinarTorta.length; i++) {
+  let pasos = cocinarTorta[i];
+  console.log(pasos);
+}
+
+//EJERCIO IR AL MERCADO
+
+var irAlMercado = [
+  "1-Salir de casa con bolsa y billetera",
+  "2-Caminar hasta la esquina",
+  "3-Doblar a la derecha",
+  "4-En 30 metros se encuentra el super"
+];
+for (let i = 0; i < irAlMercado.length; i++) {
+  let pasos = irAlMercado[i];
+  console.log(pasos);
+}
